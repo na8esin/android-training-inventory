79f2ef34e929d2adfce1113ae737187e8a292f50
渡辺孝之 <na8esin.mpst@gmail.com>
2021-08-03 12:22:47 +0900

    In ItemListFragment, Add a lambda to navigate to the details screen as a constructor parameter to the ItemListAdapter{}.

--- app/src/main/java/com/example/inventory/ItemListFragment.kt
+++ app/src/main/java/com/example/inventory/ItemListFragment.kt
@@ -51,6 +51,9 @@ class ItemListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val adapter = ItemListAdapter {
+            val action =
+                ItemListFragmentDirections.actionItemListFragmentToItemDetailFragment(it.id)
+            this.findNavController().navigate(action)
         }
         binding.recyclerView.adapter = adapter
         viewModel.allItems.observe(this.viewLifecycleOwner) { items ->
